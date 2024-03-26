45adc0812f6414990f813e3b5efa87f32e9a3518
ejsamuel <ejsamuel@ncsu.edu>
2024-03-26 12:36:53 -0400

    trying curl requests for release

--- TestEase/postbuild.sh
+++ TestEase/postbuild.sh
@@ -1,16 +1,19 @@
 echo "Compressing artifacts into one file" 
 powershell Compress-Archive -Path "C:\ProgramData\Jenkins\.jenkins\workspace\TestEaseBuild\TestEase\TestEase\bin\x64\Release\net8.0-windows10.0.19041.0\win10-x64\publish\*" -DestinationPath publish.zip
 
-echo "Exporting token and enterprise api to enable github-release tool" 
+echo "Creating a new release in github" 
 set GITHUB_TOKEN=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 
 set GITHUB_API=https://github.ncsu.edu/api/v3/
+set REPO=2024SpringTeam31-Hitachi-2
+set USER=ejsamuel
+set TAG=v0
+set NAME="Release Name"
 
-#echo "Deleting release from github before creating new one" 
-#github-release delete --user "ejsamuel" --repo "2024SpringTeam31-Hitachi-2" --tag "v0" --security-token %GITHUB_TOKEN%
-
-echo "Creating a new release in github" 
-github-release release --user "ejsamuel" --repo "2024SpringTeam31-Hitachi-2" --tag "v0" --name "Release Name" --security-token %GITHUB_TOKEN%
+curl -X POST -H "Authorization: token %GITHUB_TOKEN%" -d "{ \"tag_name\": \"%TAG%\", \"target_commitish\": \"master\", \"name\": \"%NAME%\", \"body\": \"Description of the release\", \"draft\": false, \"prerelease\": false }" %GITHUB_API%/repos/%USER%/%REPO%/releases
 
 echo "Uploading the artifacts into github" 
-github-release upload --user "ejsamuel" --repo "2024SpringTeam31-Hitachi-2" --tag "v0" --name "publish.zip" --file publish.zip --security-token %GITHUB_TOKEN%
+set FILE=publish.zip
+
+curl -X POST -H "Authorization: token %GITHUB_TOKEN%" -H "Content-Type: $(file -b --mime-type %FILE%)" --data-binary @%FILE% %GITHUB_API%/repos/%USER%/%REPO%/releases/%TAG%/assets?name=$(basename %FILE%)
+
 
