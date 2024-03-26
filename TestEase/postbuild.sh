c7e19381a2a0ce7f219a678cfa1c2f40360aab42
ejsamuel <ejsamuel@ncsu.edu>
2024-03-26 12:02:44 -0400

    updating post build script to accomodate windows and work for enterprise github

--- TestEase/postbuild.sh
+++ TestEase/postbuild.sh
@@ -1,15 +1,15 @@
-echo "Compressing artifacts into one file"
-zip -r publish.zip ./publish
+echo "Compressing artifacts into one file" 
+powershell Compress-Archive -Path ./publish/* -DestinationPath publish.zip
 
-echo "Exporting token and enterprise api to enable github-release tool"
-export GITHUB_TOKEN=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
-export GITHUB_API=https://api.github.com # needed only for enterprise
+echo "Exporting token and enterprise api to enable github-release tool" 
+set GITHUB_TOKEN=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 
+set GITHUB_API=https://github.ncsu.edu/api/v3/
 
-echo "Deleting release from github before creating new one"
-github-release delete --user "ejsamuel" --repo "2024SpringTeam31-Hitachi-2" --tag "v0"	
+echo "Deleting release from github before creating new one" 
+github-release delete --user "ejsamuel" --repo "2024SpringTeam31-Hitachi-2" --tag "v0"
 
-echo "Creating a new release in github"
+echo "Creating a new release in github" 
 github-release release --user "ejsamuel" --repo "2024SpringTeam31-Hitachi-2" --tag "v0" --name "Release Name"
 
-echo "Uploading the artifacts into github"
+echo "Uploading the artifacts into github" 
 github-release upload --user "ejsamuel" --repo "2024SpringTeam31-Hitachi-" --tag "v0" --name "publish.zip" --file publish.zip
