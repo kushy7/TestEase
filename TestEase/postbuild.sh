dde17aee6e0037cf61a6d1059b7b0f3b9d90f223
ejsamuel <ejsamuel@ncsu.edu>
2024-03-27 11:00:03 -0400

    dynamic script to upload to new release.

--- TestEase/postbuild.sh
+++ TestEase/postbuild.sh
@@ -1,16 +1,22 @@
-echo "Compressing artifacts into one file" 
+echo "Compressing artifacts into one file"
 powershell Compress-Archive -Path "C:\ProgramData\Jenkins\.jenkins\workspace\TestEaseBuild\TestEase\TestEase\bin\x64\Release\net8.0-windows10.0.19041.0\win10-x64\publish\*" -DestinationPath publish.zip
 
-echo "Creating a new release in github" 
-set GITHUB_TOKEN=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 
+echo "Creating a new release in GitHub"
+set GITHUB_TOKEN=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
 set GITHUB_API=https://github.ncsu.edu/api/v3/
 set REPO=2024SpringTeam31-Hitachi-2
 set ORG=engr-csc-sdc
 set TAG=v0
 set NAME="Release Name"
 
-curl -X POST -H "Authorization: token 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 " -d "{ \"tag_name\": \"v0\", \"target_commitish\": \"jenkins-publish\", \"name\": \"Release Name\", \"body\": \"Description of the release\", \"draft\": false, \"prerelease\": false }" https://github.ncsu.edu/api/v3/repos/engr-csc-sdc/2024SpringTeam31-Hitachi-2/releases
-echo "Uploading the artifacts into github" 
+# Create the release and capture the release ID
+RELEASE_RESPONSE=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" -d "{ \"tag_name\": \"$TAG\", \"target_commitish\": \"jenkins-publish\", \"name\": $NAME, \"body\": \"Description of the release\", \"draft\": false, \"prerelease\": false }" "https://github.ncsu.edu/api/v3/repos/$ORG/$REPO/releases")
+RELEASE_ID=$(echo $RELEASE_RESPONSE | jq -r '.id')
+
+echo "Release created with ID: $RELEASE_ID"
+
+echo "Uploading the artifacts into GitHub"
 set FILE=publish.zip
 
-curl -X POST -H "Authorization: token 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" -H "Content-Type: application/zip" --data-binary @publish.zip "https://github.ncsu.edu/api/uploads/repos/engr-csc-sdc/2024SpringTeam31-Hitachi-2/releases/2160/assets?name=publish.zip"
+# Use the obtained release ID to upload the artifact
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/zip" --data-binary @$FILE "$GITHUB_API/repos/$ORG/$REPO/releases/$RELEASE_ID/assets?name=$FILE"
