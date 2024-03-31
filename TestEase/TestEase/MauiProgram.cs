b3901048b4d1aaf290b4cba6b4f9ea42d2f3b4f8
Kush <kppatel7@ncsu.edu>
2024-03-31 03:48:46 -0400

    Application is maximized on startup addresses the issue #20

--- TestEase/TestEase/MauiProgram.cs
+++ TestEase/TestEase/MauiProgram.cs
@@ -4,6 +4,7 @@ using TestEase.Views;
 using CommunityToolkit.Maui;
 using TestEase.Services;
 using Microsoft.Maui.Platform;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace TestEase
 {
@@ -17,6 +18,31 @@ namespace TestEase
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             }).UseMauiCommunityToolkit();
+
+
+
+            //code to maximize application on startup
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated(window =>
+                    {
+                        IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        Microsoft.UI.WindowId win32WindowsId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                        Microsoft.UI.Windowing.AppWindow winuiAppWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(win32WindowsId);
+                        if (winuiAppWindow.Presenter is Microsoft.UI.Windowing.OverlappedPresenter p)
+                        {
+                            p.Maximize();
+                        }
+                    });
+                });
+            });
+#endif
+
+
+
 
 #if DEBUG
             builder.Logging.AddDebug();
